fc495c8fdfafd7b02255999cd8a65f60459aa8bf
Danish Khan <dkhan1010@gmail.com>
2024-08-25 08:19:41 -0700

    Create prime.py

---
+++ example_code/prime.py
@@ -0,0 +1,17 @@
+# declaration or setting parameters
+i = 1
+list = [i for i in range(3, 400, 2)]
+p = [2, 3]
+#print(list)
+
+while True:
+    while p[len(p) - 1] * i < max(list):
+        i = i + 1
+        if p[len(p) - 1] * i in list:
+            list.remove(p[len(p) - 1] * i)
+    list.remove(list[0])
+    p.append(list[0])
+    if len(list) == 1:
+        break
+#print(list)
+print("Prime numbers:",p)
