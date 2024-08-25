560ea6d6ecc6dd2bf24f040da207fe13f7d1a615
Danish Khan <dkhan1010@gmail.com>
2024-08-25 08:34:42 -0700

    Update prime.py

--- example_code/prime.py
+++ example_code/prime.py
@@ -1,4 +1,3 @@
-# slight error in the prime number generation. I will update here. This is a general idea.
 # declaration or setting parameters
 i = 1
 list = [i for i in range(3, 400, 2)]
@@ -10,6 +9,7 @@ while True:
         i = i + 1
         if p[len(p) - 1] * i in list:
             list.remove(p[len(p) - 1] * i)
+    i = 1
     list.remove(list[0])
     p.append(list[0])
     if len(list) == 1:
